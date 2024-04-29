82ffc521a355a9d1a22d969ecf0232ff1a6e98a2
송민선 <poweron0335@gmail.com>
2024-04-29 15:45:06 +0900

    save image in server folder

---
+++ movie/src/main/resources/static/js/register.js
@@ -0,0 +1,29 @@
+// x를 누르면 파일 삭제 요청
+// a 태그 기능 중지 => href 값 가져와서 화면 출력
+// 만들어진 태그는 이벤트 불가능하여 output 갖고오기
+document.querySelector('.uploadResult').addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('x 클릭', e.target); // <i class="fa-solid fa-x"></i>
+
+  console.log('x 클릭', e.currentTarget);
+
+  const currentLi = e.target.closest('li');
+
+  // data-file 에 있는 값 가져오기
+  const filePath = e.target.closest('a').dataset.file; // data-file 에 있는 값을 가져올 땐 무조건 dataset."파일명"
+  console.log('filePath', filePath);
+
+  const formData = new FormData();
+  formData.append('filePath', filePath);
+
+  fetch('/upload/remove', { method: 'post', body: formData })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      if (data) {
+        // 해당 div 제거
+        currentLi.remove();
+      }
+    });
+});
--- movie/src/main/resources/static/js/upload.js
+++ movie/src/main/resources/static/js/upload.js
@@ -1,6 +1,40 @@
 // fileInput 찾기
 const fileInput = document.querySelector('#fileInput');
 
+// 업로드 파일 보여주기 찾기
+const uploadResult = document.querySelector('.uploadResult ul');
+
+function checkExtension(fileName) {
+  // 정규식 사용
+  const regex = /(.*?).(png|gif|jpg)$/;
+
+  // txt => false, 이미지 => true
+  console.log(regex.test(fileName));
+
+  return regex.test(fileName);
+}
+
+function showUploadImages(arr) {
+  console.log('showUploadImages', arr);
+
+  let tags = '';
+
+  arr.forEach((obj, idx) => {
+    tags += `<li data-name="${obj.fileName}" data-path="${obj.folderPath}" data-uuid="${obj.uuid}">`;
+    tags += `<div>`;
+    tags += `<a href=""><img src="/upload/display?fileName=${obj.thumbImageURL}" class="block"></a>`;
+    tags += `<span class="text-sm d-inline-block mx-1">${obj.fileName}</span>`;
+    tags += `<a href="#" data-file="${obj.imageURL}">`;
+    tags += `<i class="fa-solid fa-xmark"></i></a>`;
+    tags += `</div></li>`;
+  });
+  uploadResult.insertAdjacentHTML('beforeend', tags);
+}
+
+// fileInput change 이벤트 걸기
+// checkExtension() 호출
+// 이미지 파일이라면 FormData() 객체 생성 후
+// append
 fileInput.addEventListener('change', (e) => {
   const files = e.target.files;
 
@@ -15,19 +49,14 @@ fileInput.addEventListener('change', (e) => {
   for (const value of formData.values()) {
     console.log(value);
   }
+  fetch('/upload/uploadAjax', {
+    method: 'post',
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      showUploadImages(data);
+    });
 });
-
-function checkExtension(fileName) {
-  // 정규식 사용
-  const regex = /(.*?).(png|gif|jpg)$/;
-
-  // txt => false, 이미지 => true
-  console.log(regex.test(fileName));
-
-  return regex.test(fileName);
-}
-
-// fileInput change 이벤트 걸기
-// checkExtension() 호출
-// 이미지 파일이라면 FormData() 객체 생성 후
-// append
